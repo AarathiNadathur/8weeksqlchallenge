3c265bf80325d50dc96e786e70c59b115a4e4277
Aarathi N <44685429+AarathiNadathur@users.noreply.github.com>
2021-05-24 19:08:53 -0400

    Delete 6.sql

--- 6.sql
+++
@@ -1,8 +0,0 @@
-# Which item was purchased first by the customer after they became a member?
-with cte as (
-select s.*,datediff(s.order_date, me.join_date) as datedif
-from sales s join members me on 
-s.customer_id = me.customer_id
-)
-select cte.customer_id,m.product_id,m.product_name , min(cte.datedif)
-from cte inner join menu m on cte.product_id=m.product_id where cte.datedif>0 group by cte.customer_id;
