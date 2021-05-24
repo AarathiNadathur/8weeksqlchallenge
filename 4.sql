a2c5f13e25d8ffb82c93f0d86e7e9cda09aa06fa
Aarathi N <44685429+AarathiNadathur@users.noreply.github.com>
2021-05-24 19:08:43 -0400

    Delete 4.sql

--- 4.sql
+++
@@ -1,6 +0,0 @@
-# What is the most purchased item on the menu and how many times was it purchased by all customers?
-select m.product_name, count(s.customer_id)as times_purchased
-from sales s join menu m 
-on s.product_id=m.product_id
-group by m.product_id
-order by times_purchased desc limit 1;
