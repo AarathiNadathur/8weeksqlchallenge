0d94edfafd3f0d762e3aefbe091c07701d9b623e
Aarathi N <44685429+AarathiNadathur@users.noreply.github.com>
2021-05-24 19:07:57 -0400

    Delete 1.sql

--- 1.sql
+++
@@ -1,5 +0,0 @@
-# What is the total amount each customer spent at the restaurant?
-Select s.customer_id as Customer, sum(m.price) as total_bill 
-from sales  s join  menu m 
-on s.product_id=m.product_id 
-group by Customer;
